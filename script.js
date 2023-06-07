66fcf98299631138f5c1bf719e36454bedabec99
dielawn <dielawnmerkill@gmail.com>
2023-06-07 06:23:17 -0600

    constructor function for todo

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+//factory functions
 
-
-// Todo list = new Todo();
-// Todo school = new HighSchool();
-
-
-function personFactory (name) {
-   return {
+function createPerson(name) {
+    return {
         name,
-        talk()  {
-            return `Hello I am ${name}`
+        talk() {
+return `I am ${this.name}`
         }
     }
 }
+const me = createPerson('Sina')
+const you = createPerson('Bob')
 
+const myCoolProto = {
+    talk() {
+        return `Hello, I am ${this.name}! This is my cool proto.`
+    }
+}
 
+function createHuman(name) {
+    return Object.create(myCoolProto, {
+        name: {
+            value: name
+        }
+    })
+}
+
+const someone = createHuman('Sally')
 
 function createElement(type, text, color){
     const el = document.createElement(type)
@@ -58,19 +70,52 @@ function createToDo(title, description, dueDate, priority) {
 }
 let description = 'Teeth cleaning and x-ray'
 const firstEvent = createToDo('Dentist', description, '11/24/23', 'High' )
-// const me = {
-//     name: 'dielawn',
-//     talk() {
-//         return `Hello I am ${this.name}`
-//     }
-// }
 
-// const alpal = {
-//     name: 'Alpal',
-//     talk() {
-//         return `Hello I am ${this.name}`
-//     }
-// }
 
-// Todo list = ListFactory.Construct(); // new ElementarySchool() inside
+//Constructor function
+function ToDoItem(title, description, dueDate, priority) {
+    this.title = title,
+    this.description = description,
+    this.dueDate = dueDate,
+    this.priority = priority,
+    this.log = () => {
+        return  `${this.title}, ${this.description}, ${dueDate}, ${priority}`
+    }
+}
+
+const drAppt = new ToDoItem('Dr. Appt', 'Minor procedure', '5/15/23', 'High')
+
+function Person(name, age, height) {
+    this.name = name,
+    this.age = age,
+    this.height = height
+    this.talk = () => {
+        return `Hello I am ${this.name}`
+    }
+}
+
+Person.prototype.talk = function() {
+    return `Ciao, io sono ${this.name}`
+}
+
+const dielawn = new Person('Dielawn', '39', '5ft, 10in')
+const alpal = new Person('Alpal', '33', '5ft, 10in')
+const hildy = new Person('Brumhilda', '3.5', '2ft')
+
+
+function SuperElement(type, content) {
+    this.el = document.createElement(type)
+    this.el.innerText = content
+    document.body.append(this.el)
+    this.el.addEventListener('click', () => {
+        console.log(this.el)
+    })
+} 
+
+const h2 = new SuperElement('h2', 'helloo!')
+
+const array = ['a', 'b', 'c']
+const myElements = array.map(item => {
+    return new SuperElement('p', item)
+})
 
