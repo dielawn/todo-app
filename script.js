4b34f7a4076810102263a8832d010c8a202d2875
dielawn <dielawnmerkill@gmail.com>
2023-06-07 19:45:28 -0600

    setArray function for DOM function to render, with rough category constructor function

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-//factory functions
+// //factory functions
 
-function createPerson(name) {
-    return {
-        name,
-        talk() {
-return `I am ${this.name}`
-        }
-    }
-}
-const me = createPerson('Sina')
-const you = createPerson('Bob')
+// function createPerson(name) {
+//     return {
+//         name,
+//         talk() {
+// return `I am ${this.name}`
+//         }
+//     }
+// }
+// const me = createPerson('Sina')
+// const you = createPerson('Bob')
 
-const myCoolProto = {
-    talk() {
-        return `Hello, I am ${this.name}! This is my cool proto.`
-    }
-}
+// const myCoolProto = {
+//     talk() {
+//         return `Hello, I am ${this.name}! This is my cool proto.`
+//     }
+// }
 
-function createHuman(name) {
-    return Object.create(myCoolProto, {
-        name: {
-            value: name
-        }
-    })
-}
+// function createHuman(name) {
+//     return Object.create(myCoolProto, {
+//         name: {
+//             value: name
+//         }
+//     })
+// }
 
-const someone = createHuman('Sally')
+// const someone = createHuman('Sally')
 
-function createElement(type, text, color){
-    const el = document.createElement(type)
-    el.innerText = text
-    el.style.color = color
-    document.body.append(el)
-    return{
-        el,
-        setText(text) {
-            el.innerText = text
-        },
-        setColor(color) {
-            el.style.color = color
-        }
-    }
-}
+// function createElement(type, text, color){
+//     const el = document.createElement(type)
+//     el.innerText = text
+//     el.style.color = color
+//     document.body.append(el)
+//     return{
+//         el,
+//         setText(text) {
+//             el.innerText = text
+//         },
+//         setColor(color) {
+//             el.style.color = color
+//         }
+//     }
+// }
 
-const h1 = createElement('h1', 'Hey guys', 'red')
+// const h1 = createElement('h1', 'Hey guys', 'red')
 
-function createToDo(title, description, dueDate, priority) {
+function createToDo(title, description, dueDate, time, priority) {
     const toDo = {
         title: title,
         description: description,
         dueDate: dueDate,
+        time: time,
         priority: priority
     }
     return{
@@ -63,27 +64,38 @@ function createToDo(title, description, dueDate, priority) {
         setDueDate(dueDate) {
             toDo.dueDate = dueDate
         },
+        setTime(time) {
+            toDo.time = time
+        },
         setPriority(priority) {
             toDo.priority = priority
-        }
-    }
-}
-let description = 'Teeth cleaning and x-ray'
-const firstEvent = createToDo('Dentist', description, '11/24/23', 'High' )
+        },
+        setArray(array) {
+            array.push (toDo.title, toDo.description, toDo.dueDate, toDo.time, toDo.priority)
+         }
+     }
+ }
+ let description = 'Teeth cleaning and x-ray'
+ let firstEvent = createToDo('Dentist', description, '11/24/23', '12:00', 'High' )
+ let secondEvent = createToDo('Hair did', 'Pubes', '11/25/23', '1:00', 'Very High' )
+ 
+ let testArray = []
+// const domArray = [toDo.title, toDo.description, toDo.dueDate, toDo.time, toDo.priority]
 
 
 //Constructor function
-function ToDoItem(title, description, dueDate, priority) {
-    this.title = title,
+function Category(category, description, ) {
+    this.category = category,
     this.description = description,
-    this.dueDate = dueDate,
-    this.priority = priority,
-    this.log = () => {
-        return  `${this.title}, ${this.description}, ${dueDate}, ${priority}`
-    }
+    // this.log = () => {
+    //     return  `${this.category}, ${this.description}`
+    // }
+    this.array = []
 }
 
-const drAppt = new ToDoItem('Dr. Appt', 'Minor procedure', '5/15/23', 'High')
+const work = new Category('Work', 'Professional', )
+const edu = new Category('School', 'College')
+const appt = new Category('Appointment', 'dr, dentist, plumber, etc')
 
 function Person(name, age, height) {
     this.name = name,
@@ -111,11 +123,16 @@ function SuperElement(type, content) {
         console.log(this.el)
     })
 } 
+const renderToDos = (array) => {
+    array.map(item => {
+        return new SuperElement('p', item)
+    })
+}
 
-const h2 = new SuperElement('h2', 'helloo!')
 
-const array = ['a', 'b', 'c']
-const myElements = array.map(item => {
+const h2 = new SuperElement('h2', 'Check the console')
+
+const eventElements = testArray.map(item => {
     return new SuperElement('p', item)
 })
 
