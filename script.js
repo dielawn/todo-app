957ab35b43711f8c407b4dff9cf2971ff82bc445
dielawn <dielawnmerkill@gmail.com>
2023-06-11 19:40:01 -0600

    id for removeBtns working

--- script.js
+++ script.js
@@ -32,16 +32,17 @@ function createNote(title, description, dueDate, time, priority) {
         },        
      }
  }
+ const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
 
+
+ //test variables 
  let description = 'Teeth cleaning and x-ray'
-
-
-
  const defaultNotes = [
     createNote('Dentist', description, '11/24/23', '12:00', 'High' ), 
     createNote('Hair did', 'Pubes', '11/25/23', '1:00', 'Very High' )
 ]
-
 const welcomeArray = ['Welcome', 'The bestest todo list app', 'firstEvent.setArray(work.array)', 'renderNotes(work.array)']
 
 
@@ -64,25 +65,24 @@ function SuperElement(parent, type, content, className, id) {
 
     function createList(name) {
         SuperElement(containerDiv, 'div', '', name, name)
-        const newList = document.getElementById(name)
-        SuperElement(newList, 'p', name, `list${name}`, `list${name}`)
-     
+        const newList = document.getElementById(name)       
+         let capitalName = capitalizeFirstLetter(name)
+        SuperElement(newList, 'p', capitalName, `list${name}`, `list${name}`)
+        const listNameTxt = document.getElementById(`list${name}`)
+        listNameTxt.style.fontWeight = 900;
       } 
         const defaultList = createList('general')
-        const work = createList('pro')
+        const work = createList('professional')
         const edu = createList('school')
         const appt = createList('appointment')
    
 
         const renderNotes = (array, parent) => {
-            const noteId = `note-${Date.now()}`; // Generate a unique ID for the note
-        
-            const noteDiv = new SuperElement(parent, 'div', '', 'noteDiv', noteId);
-        
+            const noteId = `note-${Date.now()}`; // Generate a unique ID for the note        
+            const noteDiv = new SuperElement(parent, 'div', '', 'noteDiv', noteId);        
             array.map(item => {
                 new SuperElement(noteDiv.el, 'p', item, 'listDiv', '');
-            });
-        
+            });        
             const removeBtn = new SuperElement(parent, 'button', 'X', 'removeBtn', noteId);
             removeBtn.el.addEventListener('click', () => {
                 console.log('remove');
@@ -93,34 +93,24 @@ function SuperElement(parent, type, content, className, id) {
                 removeBtn.el.remove(); // Remove the remove button element from the DOM
             });
         };
-        
+               
+// renderNotes(welcomeArray, pro)
+// renderNotes(welcomeArray, school)
+// renderNotes(welcomeArray, general)
+// renderNotes(welcomeArray, appointment)
 
-// const renderNotes = (array, parent) => {
-//     array.map(item => {
-//         const noteDiv = new SuperElement(parent, 'div', '', 'noteDiv', '');
-//         new SuperElement(noteDiv.el, 'p', item, 'listDiv', '');
-//     });
-
-//     const removeBtn = new SuperElement(parent, 'button', 'X', 'removeBtn', 'removeBtn');
-//     removeBtn.el.addEventListener('click', () => {
-//         console.log('remove');
-//         const noteDivs = document.getElementsByClassName('noteDiv');
-//         for (let i = noteDivs.length - 1; i >= 0; i--) {
-//             const noteDiv = noteDivs[i];
-//             noteDiv.remove(); // Remove the note element from the DOM
-//         }
-//         removeBtn.el.remove(); // Remove the remove button element from the DOM
-//     });
-// };
-
-        
-        
-renderNotes(welcomeArray, pro)
-renderNotes(welcomeArray, school)
-renderNotes(welcomeArray, general)
-renderNotes(welcomeArray, appointment)
-
-
+setTimeout(() => {
+    renderNotes(welcomeArray, school)
+    setTimeout(() => {
+        renderNotes(welcomeArray, general)
+        setTimeout(() => {
+            renderNotes(welcomeArray, professional)
+            setTimeout(() => {
+                renderNotes(welcomeArray, appointment)
+              }, 1);
+          }, 1);
+      }, 1);
+  }, 1);
 
 
 
