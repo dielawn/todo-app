704cf37b2dad9c363e71ca423dfb55335bf867b2
dielawn <dielawnmerkill@gmail.com>
2023-06-17 19:46:12 -0600

    simplified renderNoteInputs function, added renderListSelector function that pushes to lists and updates listSelect

--- script.js
+++ script.js
@@ -28,28 +28,31 @@ function createNote(title, description, dueDate, time, priority, list) {
         },
         setPriority(priority) {
             note.priority = priority
-        },
-        setList(list) {
-            note.list = note.title.push(list)
-        }            
+        },           
      }
  }
- const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  };
 
-  const lists = []
+  const lists = ['all', 'personal', 'professional']
   const createNewList = (name) => {
       lists.push(name)
   }
+
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  };
+
  //test variables 
  let description = 'Teeth cleaning and x-ray'
-const dentistApt = createNote('Dentist', description, '11/24/23', '12:00', 'High' )
-const hairApt = createNote('Hair did', 'Pubes', '11/25/23', '1:00', 'Very High' )
+const dentistApt = createNote('Dentist', description, '11/24/23', '12:00', 'High', 'All')
+const hairApt = createNote('Hair did', 'Pubes', '11/25/23', '1:00', 'Very High', 'Personal' )
 const welcomeArray = ['Welcome', 'The bestest todo list app', 'firstEvent.setArray(work.array)', 'renderNotes(work.array)']
 
 
 //DOM stuff
+const containerDiv = document.getElementById('container')
+const listDiv = document.getElementById('listDiv')
+const inputDiv = document.getElementById('inputDiv')
+
 class SuperElement {
     constructor(parent, elementType, innerHTML = '', className = '', id = '') {
       const element = document.createElement(elementType);
@@ -65,177 +68,193 @@ class SuperElement {
     }
   }
   
-//     const bodyElement = document.querySelector('body')
-//     const newContainer = new SuperElement(bodyElement, 'div', '', 'container', 'container')
-//     const containerDiv = document.getElementById('container')
 
+ let displayedList
+ const renderList = (array) => {
 
- 
-//  let generalList = createNewList('general')
-
-
-//  let displayedList
-//  const renderList = (array) => {
-//     new SuperElement(containerDiv, 'div', '', 'listDiv', 'listDiv');
-//     const listDiv = document.getElementById('listDiv');
-//     let displayedList = null;
+    array.map(item => {
+      let capitalName = capitalizeFirstLetter(item);
+      const listHeaderId = item.toLowerCase() + 'Header';
+      let listHeader = document.getElementById(listHeaderId);
   
-//     array.map(item => {
-//       let capitalName = capitalizeFirstLetter(item);
-//       const listHeaderId = item.toLowerCase() + 'Header';
-//       let listHeader = document.getElementById(listHeaderId);
+      if (!listHeader) {
+        new SuperElement(listDiv, 'div', capitalName, 'listHeader', listHeaderId);
+        new SuperElement(listDiv, 'div', '', 'list', item);
+        listHeader = document.getElementById(listHeaderId);
+      }
   
-//       if (!listHeader) {
-//         new SuperElement(listDiv, 'div', capitalName, 'listHeader', listHeaderId);
-//         new SuperElement(listDiv, 'div', '', 'list', item);
-//         listHeader = document.getElementById(listHeaderId);
-//       }
+      listHeader.addEventListener('click', () => {
+        displayedList = listHeader.id; // Assign the ID of the clicked list header
+        hideListNotesExcept(displayedList);
+      });
+    });
   
-//       listHeader.addEventListener('click', () => {
-//         displayedList = listHeader.id; // Assign the ID of the clicked list header
-//         hideListNotesExcept(displayedList);
-//       });
-//     });
+    return displayedList;
+  };
   
-//     return displayedList;
-//   };
-  
-  
-
-
-   
-
-// const renderNotes = (notes, parent, callback) => {
-//     const noteId = `note-${Date.now()}`
-//     const noteDiv = new SuperElement(parent, 'div', '', 'noteDiv', noteId)        
-//     for (let i = 0; i < notes.length; i++) {
-//       new SuperElement(noteDiv.el, 'p', notes[i], 'note', '')
-//     }    
-//     const removeBtn = new SuperElement(parent, 'button', 'X', 'removeBtn', noteId)
-//     removeBtn.el.addEventListener('click', () => {
-//       console.log('remove')
-//       const noteToRemove = document.getElementById(noteId);
-//       if (noteToRemove) {
-//         noteToRemove.remove()
-//       }
-//       removeBtn.el.remove()
-//     });
+const renderNotes = (notes, parent, callback) => {
+    const noteId = `note-${Date.now()}`
+    const noteDiv = new SuperElement(parent, 'div', '', 'noteDiv', noteId)        
+    for (let i = 0; i < notes.length; i++) {
+      new SuperElement(noteDiv.el, 'p', notes[i], 'note', '')
+    }    
+    const removeBtn = new SuperElement(parent, 'button', 'X', 'removeBtn', noteId)
+    removeBtn.el.addEventListener('click', () => {
+      console.log('remove')
+      const noteToRemove = document.getElementById(noteId);
+      if (noteToRemove) {
+        noteToRemove.remove()
+      }
+      removeBtn.el.remove()
+    });
     
-//     if (typeof callback === 'function') {
-//       callback(); // Execute the callback function if provided
-//     }
-//   }
-               
-// const renderNoteInputs = () => {
-//     new SuperElement(containerDiv, 'div', '', 'inputDiv', 'inputDiv')
-//     const inputDiv = document.getElementById('inputDiv')
-//     inputDiv.classList.add('flexColumn')
-//         //Title input
-//         new SuperElement(inputDiv, 'input', '', 'titleInput', 'titleInput')
-//         const titleInput = document.getElementById('titleInput')
-//         titleInput.placeholder = 'Title'
-//         // Description input
-//         new SuperElement(inputDiv, 'input', '', 'descInput', 'descInput')
-//         const descInput = document.getElementById('descInput')
-//         descInput.placeholder = 'Description'
-//         //Date input
-//         new SuperElement(inputDiv, 'input', '', 'dateInput', 'dateInput')
-//         const dateInput = document.getElementById('dateInput')
-//         dateInput.placeholder = 'Date'
-//         //Time input
-//         new SuperElement(inputDiv, 'input', '', 'timeInput', 'timeInput')
-//         const timeInput = document.getElementById('timeInput')
-//         timeInput.placeholder = 'Time'
-//         //Priority Select
-//         new SuperElement(inputDiv, 'label', 'Priority', 'prioritySelectLabel', 'prioritySelectLabel')
-//         const prioritySelectLabel = document.getElementById('prioritySelectLabel')
-//         prioritySelectLabel.for = 'prioritySelect'
-//         new SuperElement(inputDiv, 'select', '', 'prioritySelect', 'prioritySelect')
-//         const prioritySelect = document.getElementById('prioritySelect')   
-//         prioritySelect.name = 'prioritySelectLabel'    
-//         // Create option elements and set their values
-//         const lowOption = document.createElement('option')
-//             lowOption.value = 'Low'
-//             lowOption.text = 'Low'
+    if (typeof callback === 'function') {
+      callback(); // Execute the callback function if provided
+    }
+  }
+const priorityLevel = ['Low', 'Medium', 'High', 'Urgent']     
+const renderNoteInputs = () => {
+    const inputDiv = document.getElementById('inputDiv')
+    inputDiv.classList.add('flexColumn')
+        //Title input
+        new SuperElement(inputDiv, 'input', '', 'titleInput', 'titleInput')
+        const titleInput = document.getElementById('titleInput')
+        titleInput.placeholder = 'Title'
+        // Description input
+        new SuperElement(inputDiv, 'input', '', 'descInput', 'descInput')
+        const descInput = document.getElementById('descInput')
+        descInput.placeholder = 'Description'
+        //Date input
+        new SuperElement(inputDiv, 'input', '', 'dateInput', 'dateInput')
+        const dateInput = document.getElementById('dateInput')
+        dateInput.placeholder = 'Date'
+        //Time input
+        new SuperElement(inputDiv, 'input', '', 'timeInput', 'timeInput')
+        const timeInput = document.getElementById('timeInput')
+        timeInput.placeholder = 'Time'
+        //Priority Select
+        new SuperElement(inputDiv, 'div', '', 'prioritySelectDiv', 'prioritySelectDiv')
+        const prioritySelectDiv = document.getElementById('prioritySelectDiv')
+        new SuperElement(prioritySelectDiv, 'label', 'Priority', 'prioritySelectLabel', 'prioritySelectLabel')
+        const prioritySelectLabel = document.getElementById('prioritySelectLabel')
+        prioritySelectLabel.for = 'prioritySelect'
+        new SuperElement(prioritySelectDiv, 'select', '', 'prioritySelect', 'prioritySelect')
+        const prioritySelect = document.getElementById('prioritySelect')   
+        prioritySelect.name = 'prioritySelectLabel'    
+        // Create option elements and set their values
+        const options = priorityLevel.map(item => {
+            const option = document.createElement('option')
+            option.value = item
+            option.text = item
+            return option
+        })
+        // const lowOption = document.createElement('option')
+        //     lowOption.value = 'Low'
+        //     lowOption.text = 'Low'
 
-//         const mediumOption = document.createElement('option')
-//             mediumOption.value = 'Medium'
-//             mediumOption.text = 'Medium'
+        // const mediumOption = document.createElement('option')
+        //     mediumOption.value = 'Medium'
+        //     mediumOption.text = 'Medium'
 
-//         const highOption = document.createElement('option')
-//             highOption.value = 'High'
-//             highOption.text = 'High'
+        // const highOption = document.createElement('option')
+        //     highOption.value = 'High'
+        //     highOption.text = 'High'
 
-//         const urgentOption = document.createElement('option')
-//             urgentOption.value = 'Urgent'
-//             urgentOption.text = 'Urgent'
-//         // Append the option elements to the select element
-//         prioritySelect.add(lowOption)
-//         prioritySelect.add(mediumOption)
-//         prioritySelect.add(highOption)
-//         prioritySelect.add(urgentOption)
+        // const urgentOption = document.createElement('option')
+        //     urgentOption.value = 'Urgent'
+        //     urgentOption.text = 'Urgent'
+        // Append the option elements to the select element
+        options.forEach(option => {
+            prioritySelect.add(option)
+        })
+        // prioritySelect.add(lowOption)
+        // prioritySelect.add(mediumOption)
+        // prioritySelect.add(highOption)
+        // prioritySelect.add(urgentOption)
+        
+        renderListSelector()
 
-//         new SuperElement(inputDiv, 'button', 'Add Note', 'addNoteBtn', 'addNoteBtn')
-//         const addNoteBtn = document.getElementById('addNoteBtn')
-//         addNoteBtn.addEventListener('click', () => {
-//             const newNote = createNote(titleInput.value, descInput.value, dateInput.value, timeInput.value, prioritySelect.value, )
-//             console.log(displayedList)
-//             console.log(defaultList)
-//             if (displayedList === undefined) {
-//                 console.log(defaultList)
-//                 displayedList = defaultList
-//             }
-//             console.log(displayedList)
-//             renderNotes(newNote.note.array, general, () => {
-//                 console.log(displayedList)
-//             })       
-//         })
-// }
+        new SuperElement(inputDiv, 'button', 'Add Note', 'addNoteBtn', 'addNoteBtn')
+        const addNoteBtn = document.getElementById('addNoteBtn')
+        addNoteBtn.addEventListener('click', () => {
+            const newNote = createNote(titleInput.value, descInput.value, dateInput.value, timeInput.value, prioritySelect.value, )
+            console.log(displayedList)
+            console.log(defaultList)
+            if (displayedList === undefined) {
+                console.log(defaultList)
+                displayedList = defaultList
+            }
+            console.log(displayedList)
+            renderNotes(newNote.note.array, general, () => {
+                console.log(displayedList)
+            })       
+        })
+}
+const renderListSelector = () => {
+    // list selector
+    new SuperElement(inputDiv, 'div', '', 'listSelectDiv', 'listSelectDiv')
+    const listSelectDiv = document.getElementById('listSelectDiv')
+    listSelectDiv.innerHTML = ''
+    new SuperElement(listSelectDiv, 'select', '', 'listSelect', 'listSelect')
+    const listSelect = document.getElementById('listSelect')   
+    listSelect.name = 'listSelectLabel'    
+   // Create option elements and set their values
+    const options = lists.map(item => {
+    const option = document.createElement('option')
+    option.value = item
+    option.text = item
+    return option
+});
+    // Add options to the listSelect element
+    options.forEach(option => {
+    listSelect.add(option);
+});
+}
+const renderNewListInput = () => {
+    //Title input
+    new SuperElement(inputDiv, 'input', '', 'newListInput', 'newListInput')
+    const newListInput = document.getElementById('newListInput')
+    newListInput.placeholder = 'New List'
+    //Add list button
+    new SuperElement(inputDiv, 'button', 'Add List', 'addListBtn', 'addListBtn')
+        const addListBtn = document.getElementById('addListBtn')
+        addListBtn.addEventListener('click', () => {
+            const newList = createNewList(newListInput.value)            
+            console.log(newList)
+            renderList(lists)
+            renderListSelector()
+        })
+}
 
-// const renderNewListInput = () => {
-//     //Title input
-//     new SuperElement(inputDiv, 'input', '', 'newListInput', 'newListInput')
-//     const newListInput = document.getElementById('newListInput')
-//     newListInput.placeholder = 'New List'
-//     //Add list button
-//     new SuperElement(inputDiv, 'button', 'Add List', 'addListBtn', 'addListBtn')
-//         const addListBtn = document.getElementById('addListBtn')
-//         addListBtn.addEventListener('click', () => {
-//             const newList = createNewList(newListInput.value)            
-//             console.log(newList)
-//             renderList(lists)
-//         })
-// }
-
-// const hideListNotesExcept = (displayedListId) => {
-//     const listOfLists = document.querySelectorAll('.list')   
-//     for (let i = 0; i < listOfLists.length; i++) {
-//       const currentList = listOfLists[i]      
-//       if (currentList.id === displayedListId) {
-//         currentList.classList.remove('hide')
-//       } else {
-//         currentList.classList.add('hide')
-//       }
-//     }
-//   };
-//   console.log(displayedList)
-//   let defaultList = lists[0]
-//   console.log(defaultList)
-//   renderNoteInputs()
-//   renderNewListInput()
-//   document.addEventListener('DOMContentLoaded', function() {
-//     renderList(lists);
-//     hideListNotesExcept(displayedList);
+const hideListNotesExcept = (displayedListId) => {
+    const listOfLists = document.querySelectorAll('.list')   
+    for (let i = 0; i < listOfLists.length; i++) {
+      const currentList = listOfLists[i]      
+      if (currentList.id === displayedListId) {
+        currentList.classList.remove('hide')
+      } else {
+        currentList.classList.add('hide')
+      }
+    }
+  };
+  console.log(displayedList)
+  let defaultList = lists[0]
+  console.log(defaultList)
+  renderNoteInputs()
+  renderNewListInput()
+  document.addEventListener('DOMContentLoaded', function() {
+    renderList(lists);
+    hideListNotesExcept(displayedList);
  
-//     const parentElement = document.getElementById(displayedList);
-//     if (parentElement) {
-//       renderNotes(hairApt.note.array, parentElement, () => {
-//         console.log(`note rendered ${displayedList}`);
-//       });
-//     } else {
-//       console.log(`Parent element '${displayedList}' does not exist.`);
-//     }
-//   });
+    const parentElement = document.getElementById(displayedList);
+    if (parentElement) {
+      renderNotes(hairApt.note.array, parentElement, () => {
+        console.log(`note rendered ${displayedList}`);
+      });
+    } else {
+      console.log(`Parent element '${displayedList}' does not exist.`);
+    }
+  });
   
   
   
